d97f991d024a5f0ba0279334e9204474eeb0556f
jsl0149 <jsl0149@naver.com>
2022-02-13 23:14:07 +0900

    Solved : 11054

--- DP/11054/11054.cpp
+++ DP/11054/11054.cpp
@@ -4,18 +4,18 @@
 
 using namespace std;
 
+int dp[1001] = { 0, };
 
-int dp[1001];
+int dp2[1001] = { 0, };
 
-int a[1001];
-
-
+int a[1001] = { 0, };
 
 bool compare(int a, int b) {
 
 	return a > b;
 
 }
+
 
 int main() {
 
@@ -33,16 +33,14 @@ int main() {
 
 	}
 
-	dp[1] = 1;
-
-	for (int i = 2; i <= N; i++) {
+	
+	
+	for (int i = 1; i <= N; i++) {
 
 		int dp_max = 0;
 
 		for (int j = 1; j <= i; j++) {
 			if (a[i] > a[j]) {
-
-
 				if (dp[j] > dp_max) {
 					dp_max = dp[j];
 					dp[i] = dp_max;
@@ -50,22 +48,47 @@ int main() {
 			}
 		}
 
-		
+
 
 		dp[i] = dp[i] + 1;
 
 	}
 
-	for (int i = 0; i <= N; i++) {
-		cout << dp[i] << " ";
+	for (int i = N; i >= 1; i--) {
+
+		int dp_max = 0;
+
+		for (int j = N; j >=i; j--) {
+			if (a[i] > a[j]) {
+				if (dp2[j] > dp_max) {
+					dp_max = dp2[j];
+					dp2[i] = dp_max;
+				}
+			}
+		}
+
+		dp2[i] += 1;
+
 	}
 
-	cout << endl;
 
-	sort(dp, dp + 1001, compare);
+	int answer[1001] = { 0, };
+
+
+	for (int i = 0; i <= N; i++) {
+		answer[i] = dp[i] + dp2[i] - 1;
+	}
 
 	
-	cout << dp[0] << "\n";
+
+
+	sort(answer, answer + 1001, compare);
+
+
+
+	cout << answer[0] << endl;
+	
+	
 
 
 }
