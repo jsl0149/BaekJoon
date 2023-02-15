b9045bf6d1a53dd27fe7a0e86628e0bd2ee50b6b
jsl0149 <76390004+jsl0149@users.noreply.github.com>
2023-02-15 17:53:32 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.72 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -5,16 +5,8 @@ function solution(A,B){
     B.sort((a,b)=>b-a);
     
     let sum1 = 0;
-    let sum2 = 0;
     
     for(let i = 0; i<A.length; i++) sum1 += A[i] * B[i];
     
-    A.sort((a,b)=>b-a);
-    B.sort((a,b)=>a-b);
-    
-    for(let i = 0; i<A.length; i++) sum2 += A[i] * B[i];
-    
-    answer = Math.min(sum1,sum2);
-    
-    return answer;
+    return sum1;
 }
