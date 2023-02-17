b5b74aa1d6228624736f13236738aa716c152c3d
jsl0149 <76390004+jsl0149@users.noreply.github.com>
2023-02-17 17:48:54 +0900

    [level 3] Title: 숫자 게임, Time: 73.52 ms, Memory: 44.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.js
@@ -0,0 +1,19 @@
+function solution(A, B) {
+    var answer = 0;
+    A.sort((a,b)=>b-a);
+    B.sort((a,b)=>b-a);
+    
+    let cur = 0;
+    
+    for(let i = 0; i<B.length;i++){
+        while(true && cur < A.length){
+            if(B[i] > A[cur++]){
+                answer++;
+                break;
+            }
+        }
+            
+    }
+    
+    return answer;
+}
