e7332d6b701b255a064ec87df379f083bc67bf0d
jsl0149 <76390004+jsl0149@users.noreply.github.com>
2023-02-19 18:59:55 +0900

    [level 3] Title: 기지국 설치, Time: 2.92 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/lv3/12979. 기지국 설치/기지국 설치.js
+++ 프로그래머스/lv3/12979. 기지국 설치/기지국 설치.js
@@ -1,28 +1,27 @@
 function solution(n, stations, w) {
     var answer = 0;
-    
+
     const dist = [];
-    
+
     let prevStart = stations[0] - w;
-    
+
     let prevEnd = stations[0] + w;
-    
+
     stations.push(n+1+w);
-    
+
     dist.push(prevStart - 1);
-    
+
     for(let i = 1; i<stations.length;i++){
         const curStart = stations[i] - w;
         const space = curStart - prevEnd - 1;
         prevEnd = stations[i] + w;
         space > 0 ? dist.push(space) : null
     }
-    
-    
+
     dist.forEach((val)=>{
         if(val % (2*w+1) === 0) answer += (val / (2*w+1));
         else answer += Math.floor(val/(2*w+1))+1;
     })
-    
+
     return answer;
 }
