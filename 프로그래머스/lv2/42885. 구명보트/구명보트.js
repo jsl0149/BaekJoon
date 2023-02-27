62b003f51cf1a8b8924c3c428ea92f1b77177788
jsl0149 <76390004+jsl0149@users.noreply.github.com>
2023-02-27 21:35:32 +0900

    [level 2] Title: 구명보트, Time: 31.97 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.js
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.js
@@ -2,7 +2,7 @@ function solution(people, limit) {
     var answer = 0;
     let sum = 0;
     people.sort((a,b)=>a-b);
-  
+
     while(people.length){
         if(people[0] + people[people.length-1] <= limit){
             people.shift();
@@ -13,7 +13,7 @@ function solution(people, limit) {
         }
         answer++;
     }
-    
-    
+
+
     return answer;
 }
