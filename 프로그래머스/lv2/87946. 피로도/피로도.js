6050192500701cb922d49e82e50589790ebec369
jsl0149 <76390004+jsl0149@users.noreply.github.com>
2023-03-31 20:26:06 +0900

    [level 2] Title: 피로도, Time: 0.37 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87946. 피로도/피로도.js
@@ -0,0 +1,30 @@
+function solution(k, dungeons) {
+    
+    const answer = [];
+    
+    const factorial = (arr, depth, tired) => {
+        
+        if(depth === dungeons.length){
+            answer.push(depth);
+            return;
+        }
+        
+        else{
+            
+            for(let i = 0; i<arr.length; i++){
+                const temp = [...arr.slice(0,i), ...arr.slice(i+1)];
+                if(tired >= arr[i][0]) factorial(temp, depth+1,tired - arr[i][1]);
+                else {
+                    answer.push(depth);
+                }
+            }
+            
+        }
+        
+        
+    }
+    
+    factorial(dungeons, 0, k);
+    
+    return Math.max.apply(null, answer);
+}
