289d0f00a93e4a18a0f78816347c210589b1743d
jsl0149 <76390004+jsl0149@users.noreply.github.com>
2023-06-29 16:30:43 +0900

    [unrated] Title: 롤케이크 자르기, Time: 126.70 ms, Memory: 71.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.js
@@ -0,0 +1,28 @@
+function solution(topping) {
+    var answer = 0;
+    
+    const leftSet = new Map();
+    const rightSet = new Map();
+    
+    topping.forEach((val)=>{
+        const value = rightSet.get(val);
+        if(value) rightSet.set(val, value+1);
+        else rightSet.set(val,1);
+    })
+    
+    topping.forEach((val)=>{
+        const value = rightSet.get(val);
+        if(value === 1) rightSet.delete(val);
+        else rightSet.set(val, value-1); 
+            
+        const valueLeft = leftSet.get(val);
+        if(valueLeft) leftSet.set(val, value+1);
+        else leftSet.set(val,1);
+    
+        if(leftSet.size === rightSet.size) answer++;
+        
+    })
+    
+    
+    return answer;
+}
