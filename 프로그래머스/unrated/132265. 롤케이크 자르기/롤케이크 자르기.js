8883013b7c062ea3332caf09ea0526b6085aef47
jsl0149 <76390004+jsl0149@users.noreply.github.com>
2023-06-30 13:00:40 +0900

    [unrated] Title: 롤케이크 자르기, Time: 122.33 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.js
+++ 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.js
@@ -1,14 +1,28 @@
 function solution(topping) {
-  const [b1Dict, b1Arr, b2Dict, b2Arr] = [new Set(), [], new Set(), []];
-  topping.forEach((t1, i) => {
-    const t2 = topping.at(-i);
-    b1Dict.add(t1);
-    if (i !== 0) b2Dict.add(t2);
-    b1Arr.push(b1Dict.size);
-    b2Arr.push(b2Dict.size);
-  });
-  return b1Arr.reduce(
-    (acc, cur, i) => (cur === b2Arr.at(-i - 1) ? acc + 1 : acc),
-    0
-  );
+    var answer = 0;
+
+    const leftSet = new Map();
+    const rightSet = new Map();
+
+    topping.forEach((val)=>{
+        const value = rightSet.get(val);
+        if(value) rightSet.set(val, value+1);
+        else rightSet.set(val,1);
+    })
+
+    topping.forEach((val)=>{
+        const value = rightSet.get(val);
+        if(value === 1) rightSet.delete(val);
+        else rightSet.set(val, value-1); 
+
+        const valueLeft = leftSet.get(val);
+        if(valueLeft) leftSet.set(val, value+1);
+        else leftSet.set(val,1);
+
+        if(leftSet.size === rightSet.size) answer++;
+
+    })
+
+
+    return answer;
 }
