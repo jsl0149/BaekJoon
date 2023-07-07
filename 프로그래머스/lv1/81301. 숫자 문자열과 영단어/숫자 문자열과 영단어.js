3c694986b0607c5fece3ca71c162cb470c84d866
jsl0149 <76390004+jsl0149@users.noreply.github.com>
2023-07-07 12:00:11 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,10 +1,5 @@
 function solution(s) {
-    var answer = 0;
     const obj = {'zero' : 0, 'one' : 1, 'two' : 2, 'three' : 3, 'four' :4, 'five' : 5, 'six' : 6, 'seven' : 7, 'eight' : 8, 'nine' : 9};
-    
     for(let key in obj) s=s.replaceAll(key, obj[key]);
-    
-    answer = parseInt(s);
-    
-    return answer;
+    return parseInt(s);
 }
