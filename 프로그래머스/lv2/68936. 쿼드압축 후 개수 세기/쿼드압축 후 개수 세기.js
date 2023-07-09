96817a9e4fc2fad6e7e9fcb0bb63e89de4c5c555
jsl0149 <76390004+jsl0149@users.noreply.github.com>
2023-07-09 13:54:37 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 20.30 ms, Memory: 40.7 MB -BaekjoonHub

--- 프로그래머스/lv2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
+++ 프로그래머스/lv2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.js
@@ -1,15 +1,53 @@
 function solution(arr) {
-    const quadZip = (row, col, n) => {
-        if(n < 2) return arr[row][col] ? [0, 1] : [1, 0];
-        let cnt0 = 0, cnt1 = 0; n >>= 1;
-        for(let [i, j] of [[0,0],[0,1],[1,0],[1,1]]) {
-            let [zero, one] = quadZip(row+i*n, col+j*n, n);
-            cnt0 += zero;
-            cnt1 += one;
+    var answer = [];
+
+    const len = arr.length;
+
+    let one = 0;
+    let zero = 0;
+
+    arr.forEach((val)=>{
+        val.forEach((value)=>{
+            if(value) one++;
+            else zero++;
+        })
+    })
+
+    const divide = (s1,s2,e1,e2, check) => {
+
+        if(check <= 1) return;
+
+        let tempOne = 0;
+        let tempZero = 0;
+
+        for(let r = s1 ; r<s2; r++){
+            for(let c = e1; c<e2; c++){
+                arr[r][c] ? tempOne++ : tempZero++;
+            }
         }
-        if(cnt0 === 0) return [0, 1];
-        if(cnt1 === 0) return [1, 0];
-        return [cnt0, cnt1];
+
+
+        if(tempOne === check) {
+            one = one - check + 1;
+            return;
+        }
+
+
+        if(tempZero === check){
+            zero = zero - check + 1;
+            return;
+        } 
+
+        divide(s1, (s1 +s2) / 2, e1, (e1 + e2) / 2, check / 4);
+        divide(s1, (s1+s2) / 2, (e1 + e2) /2, e2, check / 4);
+        divide((s1+s2) / 2, s2, e1, (e1+e2) /2 , check / 4);
+        divide((s1 +s2) / 2, s2, (e1+e2)/2, e2,  check / 4);
+
     }
-    return quadZip(0, 0, arr.length);
+
+    divide(0, len, 0, len, len * len);
+
+    console.log(zero, one);
+
+    return [zero, one];
 }
