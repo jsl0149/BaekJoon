ee20b9a7b9b5432962856795cfb7e93291a50031
jsl0149 <76390004+jsl0149@users.noreply.github.com>
2023-07-11 10:28:40 +0900

    [level 2] Title: 문자열 압축, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.js
@@ -0,0 +1,30 @@
+function solution(s) {
+    var answer = Infinity;
+    
+    const N = s.length;
+    
+    let arr = [];
+    
+    for(let i = 1; i<=N;i++){
+        
+        arr = [];
+        
+        for(let j = 0; j<N; j+=i) arr.push(s.slice(j,j+i));
+        
+        let idx = 0;
+        let str = ``;
+        while(idx < arr.length){
+            let count = 1
+            while(arr[idx] === arr[idx+count]){
+                count++;
+            }
+            if(count !== 1) str+=`${count}${arr[idx]}`;
+            else str += arr[idx];
+            idx+=count;
+        }
+        
+        answer = Math.min(answer, str.length);
+    }
+    
+    return answer;
+}
