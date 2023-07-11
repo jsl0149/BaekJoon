3d1114adaaa56e6410b3c341124caa2547438b87
jsl0149 <76390004+jsl0149@users.noreply.github.com>
2023-07-11 12:16:52 +0900

    [level 2] Title: 문자열 압축, Time: 0.25 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.js
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.js
@@ -1,17 +1,30 @@
-const solution = s => {
-  const range = [...Array(s.length)].map((_, i) => i + 1);
-  return Math.min(...range.map(i => compress(s, i).length));
-};
+function solution(s) {
+    var answer = Infinity;
 
-const compress = (s, n) => {
-  const make = ([a, l, c]) => `${a}${c > 1 ? c : ''}${l}`;
-  return make(
-    chunk(s, n).reduce(
-      ([a, l, c], e) => e === l ? [a, l, c + 1] : [make([a, l, c]), e, 1],
-      ['', '', 0]
-    )
-  );
-};
+    const N = s.length;
 
-const chunk = (s, n) =>
-  s.length <= n ? [s] : [s.slice(0, n), ...chunk(s.slice(n), n)];
+    let arr = [];
+
+    for(let i = 1; i<=N;i++){
+
+        arr = [];
+
+        for(let j = 0; j<N; j+=i) arr.push(s.slice(j,j+i));
+
+        let idx = 0;
+        let str = ``;
+        while(idx < arr.length){
+            let count = 1
+            while(arr[idx] === arr[idx+count]){
+                count++;
+            }
+            if(count !== 1) str+=`${count}${arr[idx]}`;
+            else str += arr[idx];
+            idx+=count;
+        }
+
+        answer = Math.min(answer, str.length);
+    }
+
+    return answer;
+}
