b5c1861e759eccd3c7e761c936b0853de2b77f21
jsl0149 <76390004+jsl0149@users.noreply.github.com>
2023-07-12 11:51:55 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 5.83 ms, Memory: 36.1 MB -BaekjoonHub

--- 프로그래머스/lv3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.js
+++ 프로그래머스/lv3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.js
@@ -1,56 +1,104 @@
-function solution(n, t, m, timetable) {
-    var answer = '';
-    
-    const getTotalTime = (time)=>{
-        const [hour, minute] = time.split(':').map(Number);
-        return hour * 60 + minute;
-    }
-    
-    const getFormattedTime = (hour, minute)=>{
-        return `${hour.toString().padStart(2,'0')}:${minute.toString().padStart(2,'0')}`;
-    }
-    
-    const compare = (a,b)=>{
-        const [aHour, aMinute] = a.split(':').map(Number);
-        const [bHour, bMinute] = b.split(':').map(Number);
-        
-        const totalA = aHour * 60 + aMinute;
-        const totalB = bHour * 60 + bMinute;
+function solution (n, t, m, timetable) {
+  const buses = findBuses(n, t)
+  const timeTable = timetable.map(parseTime)
 
-        return totalA - totalB;
+  timeTable.sort(compareTime)
+
+  for (let j = 0; j < buses.length; j++) {
+    const bus = buses[j]
+    bus.crews = []
+
+    let goNext = false
+
+    while (timeTable.length) {
+      if (bus.crews.length >= m) break
+      if (goNext) break
+
+      const crew = timeTable.shift()
+
+      if (isEarlierThan(crew, bus)) {
+        bus.crews.push(crew)
+      } else {
+        timeTable.unshift(crew)
+        break
+      }
     }
-    
-    timetable.sort(compare);
-    
-    const shuttle = [[9,0]];
-    
-    for(let i = 1; i<n;i++){
-        const hour = Math.floor(t * i / 60)
-        const minute = (t * i) % 60;
-        shuttle.push([9 + hour, 0 + minute]);
+  }
+
+  let time = null
+
+  for (let i = buses.length - 1; i >= 0; i--) {
+    const bus = buses[i]
+
+    if (bus.crews.length < m) {
+      time = bus
+      break
     }
-    
-    shuttle.forEach((val,idx)=>{
-        const [shuttleHour, shuttleMinute] = val;
-        const totalShuttleTime = shuttleHour * 60 + shuttleMinute;
-        let count = m;
-        const start = [];
-        while(timetable.length && getTotalTime(timetable[0]) <= totalShuttleTime && count--){
-            start.push(timetable.shift())
-        }
-        
-        if(idx === shuttle.length-1){
-            
-            if(start.length !== m) answer = getFormattedTime(shuttleHour, shuttleMinute);
-            
-            else{
-                const [hour, minute] = start[start.length-1].split(':').map(Number);
-            
-                if(minute === 0) answer = getFormattedTime(hour-1, 59);
-                else answer = getFormattedTime(hour, minute-1);
-            }
-        }
-    })
-  
-    return answer;
-} 
+
+    const nextBus = buses[i + 1]
+    const lastCrew = bus.crews[bus.crews.length - 1]
+
+    if (nextBus) {
+      if (isEarlierThan(lastCrew, nextBus.crews[0])) {
+        time = lastCrew
+        break
+      }
+    } else {
+      time = {
+        ...lastCrew,
+      }
+      if (lastCrew.minute === 0) {
+        time.hour = lastCrew.hour - 1
+        time.minute = 59
+      } else {
+        time.minute = lastCrew.minute - 1
+      }
+      break
+    }
+  }
+
+  const hour = time.hour < 10 ? `0${time.hour}` : `${time.hour}`
+  const minute = time.minute < 10 ? `0${time.minute}` : `${time.minute}`
+
+  console.log(hour + ':' + minute)
+
+  return hour + ':' + minute
+}
+
+function isEarlierThan (a, b) {
+  if (compareTime(a, b) > 0) return false
+  return true
+}
+
+function compareTime (a, b) {
+  if (a.hour > b.hour) return 1
+  if (a.hour < b.hour) return -1
+  else if (a.minute > b.minute) return 1
+  return -1
+}
+
+function findBuses (n, t) {
+  const buses = []
+
+  for (let i = 0; i < n; i++) {
+    let minutes = i * t
+    const hours = Math.floor(minutes / 60)
+    const bus = {
+      hour: 9 + hours,
+      minute: minutes % 60
+    }
+
+    buses.push(bus)
+  }
+
+  return buses
+}
+
+function parseTime (s) {
+  const [ _hour, _minute ] = s.split(':')
+
+  return {
+    hour: Number(_hour),
+    minute: Number(_minute)
+  }
+}
