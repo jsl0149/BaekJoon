f283e7c0293f0a32576a4a1f00a46e936d4da9ca
jsl0149 <76390004+jsl0149@users.noreply.github.com>
2023-07-24 10:44:33 +0900

    [unrated] Title: 무인도 여행, Time: 0.46 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.js
+++ 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.js
@@ -1,32 +1,32 @@
 function solution(maps) {
     var answer = [];
-    const N = maps.length;
-    const M = maps[0].length;
-    const dr = [0,0,-1,1];
-    const dc = [1,-1,0,0];
-    const visited = new Array(N).fill(0).map(()=> new Array(M).fill(false));
+    const N = maps.length; // MAP 행 길이
+    const M = maps[0].length; // MAP 열 길이
+    const dr = [0,0,-1,1]; // 방향 값
+    const dc = [1,-1,0,0]; // 방향 값
+    const visited = new Array(N).fill(0).map(()=> new Array(M).fill(false)); // 방문 배열
     const map = [];
     maps.forEach((row)=>{
         map.push(row.split('').map((val)=>{
             if('0' <= val && val <= '9') return parseInt(val);
             return val;
         }));
-    })
+    }) // maps 데이터 파싱
     
-    let day = 0;
+    let day = 0; // dfs로 한 번에 탐색 가능한 휴가 날짜를 담는 변수
     
     const dfs = (r,c)=>{
         for(let i = 0; i<4;i++){
-            const nr = r + dr[i];
-            const nc = c + dc[i];
+            const nr = r + dr[i]; // 상,하,좌,우 행 좌표
+            const nc = c + dc[i]; // 상,하,좌,우 열 좌표
             
-            if(nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
+            if(nr < 0 || nr >= N || nc < 0 || nc >= M) continue; // map 밖으로 벗어나면 continue
             
             if(!visited[nr][nc] && map[nr][nc] !== 'X'){
                 visited[nr][nc] = true;
                 day += map[nr][nc]
                 dfs(nr,nc);
-            }
+            } // nr,nc를 방문하지 않았고 map이 'X'가 아니면 dfs 진행
         }
     }
     
@@ -38,11 +38,11 @@ function solution(maps) {
                 day += map[i][j]
                 dfs(i,j);
                 answer.push(day);
-            }
+            } // 한 번의 탐색이 끝나면 day 초기화
         }
     }
     
-    answer.sort((a,b)=>a-b);
+    answer.sort((a,b)=>a-b); 
     
     
     return answer.length ? answer : [-1];
