152384646a1a546db6e6e7a7e7424b94c2a600cb
jsl0149 <76390004+jsl0149@users.noreply.github.com>
2023-08-03 11:17:46 +0900

    [unrated] Title: 부대복귀, Time: 93.85 ms, Memory: 101 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132266. 부대복귀/부대복귀.js
@@ -0,0 +1,46 @@
+function solution(n, roads, sources, destination) {
+    var answer = [];
+    
+    let visited = new Array(n+1).fill(0);
+    
+    const dp = new Array(n+1).fill(Infinity);
+    
+    const graph = new Array(n+1).fill(0).map(()=>[]);
+    
+    roads.forEach((val)=>{
+        const [from, to] = val;
+        graph[from].push(to);
+        graph[to].push(from);
+    })
+    
+    const bfs = (start) => {
+        
+        const queue = [];
+        queue.push([start,0]);
+        dp[start] = 0;
+        let idx = 0;
+        while(idx !== queue.length){
+            const [curNode, dist] = queue[idx++];
+            
+            for(let i = 0; i<graph[curNode].length;i++){
+                const next = graph[curNode][i];
+                if(dist+1<dp[next]){
+                    dp[next] = dist+1;
+                    queue.push([next,dist+1]);
+                }
+            }
+            
+        }
+        
+        return -1;
+    }
+    
+    bfs(destination);
+    
+    sources.forEach((val)=>{
+        if(dp[val] === Infinity) answer.push(-1);
+        else answer.push(dp[val]);
+    })
+    
+    return answer;
+}
