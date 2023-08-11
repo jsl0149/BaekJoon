096c3f19b255bc9d021edf0c4ce16300c332e153
jsl0149 <76390004+jsl0149@users.noreply.github.com>
2023-08-11 20:31:49 +0900

    [unrated] Title: 인사고과, Time: 88.83 ms, Memory: 57.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/152995. 인사고과/인사고과.js
@@ -0,0 +1,29 @@
+function solution(scores) {
+    var answer = 1;
+    
+    const compare = (a,b) => {
+        if(a[0] === b[0]) return a[1]-b[1];
+        return b[0] - a[0];
+    }
+    
+    const target = scores[0];
+    const targetScore = scores[0][0]+scores[0][1];
+    let maxScore = 0;
+    
+    scores.sort(compare);
+    
+    for(const score of scores){
+         if(score[1] < maxScore){
+            if(score === target) return -1;
+        }
+        else{
+            maxScore = Math.max(maxScore, score[1]);
+            if(targetScore < score[0] + score[1] ) answer++;
+        }
+    }
+            
+       
+    
+
+    return answer;
+}
