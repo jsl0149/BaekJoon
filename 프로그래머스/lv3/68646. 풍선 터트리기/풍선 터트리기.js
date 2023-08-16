7c437cfd82b400105e997a76cc6656d62bab2638
jsl0149 <76390004+jsl0149@users.noreply.github.com>
2023-08-17 00:12:50 +0900

    [level 3] Title: 풍선 터트리기, Time: 332.57 ms, Memory: 150 MB -BaekjoonHub

--- 프로그래머스/lv3/68646. 풍선 터트리기/풍선 터트리기.js
+++ 프로그래머스/lv3/68646. 풍선 터트리기/풍선 터트리기.js
@@ -1,17 +1,18 @@
 function solution(a) {
     var answer = 0;
-    
     const len = a.length;
     
-    const left = new Array(len).fill(0);
-    const right = new Array(len).fill(0);
+    const left = [];
+    const right = [];
     
+
     left[0] = a[0];
-    right[len-1] = a[len-1];
+    right[a.length-1] = a[a.length-1];
     
-    for(let i = 1; i<len;i++) left[i] = Math.min(left[i-1], a[i]);
+    for(let i = 1; i<len; i++) left[i] = Math.min(left[i-1],a[i]);
     
     for(let i = len-2; i>=0; i--) right[i] = Math.min(right[i+1],a[i]);
     
     return new Set([...left,...right]).size;
+    
 }
