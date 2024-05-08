8160196c2cb26fa1a15e8f00472b5b7f7e57a7e9
jsl0149 <76390004+jsl0149@users.noreply.github.com>
2024-05-08 19:26:39 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 0.52 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.js
@@ -0,0 +1,48 @@
+function solution(expression) {
+    var answer = 0;
+    
+    const data = expression.split(/([*+-])/);
+    
+    const operand = [['*', '+', '-'], ['*', '-', '+'], ['+', '*', '-'], ['+', '-', '*'], ['-', '+', '*'], ['-', '*', '+']];
+    
+    operand.forEach((val)=>{
+        
+        let copyData = data.slice();
+    
+       
+        
+        val.forEach((oper)=>{
+            
+            const stack = [];
+            
+            for(let i = 0; i<copyData.length; i++){
+                if(copyData[i] === oper){
+                    const prev = parseInt(stack.pop());
+                    const next = parseInt(copyData[i+1]);
+                    stack.push(calculate(prev,copyData[i],next));
+                    i++;
+                }
+                else{
+                    stack.push(copyData[i]);
+                }
+            }
+            
+            copyData = stack.slice();
+        })
+
+       answer = Math.max(Math.abs(copyData[0]), answer);
+        
+  
+    })
+    
+    
+    return answer;
+}
+
+function calculate(a,oper,b){
+    if(oper === '*') return a * b;
+    
+    if(oper === '+') return a + b;
+    
+    if(oper === '-') return a - b;
+}
