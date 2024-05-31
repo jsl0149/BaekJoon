18b1e3c4f932dae0208168448f08bf246272b058
jsl0149 <76390004+jsl0149@users.noreply.github.com>
2024-06-01 00:06:26 +0900

    [level 2] Title: 양궁대회, Time: 686.37 ms, Memory: 35.8 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.js
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.js
@@ -1,7 +1,7 @@
 
 
 function solution(n, info) {
-    let answer = [];
+    var answer = [];
     
     const data = new Array(11).fill(0);
    
@@ -9,27 +9,25 @@ function solution(n, info) {
     
     const dfs = (depth, acc) => {
         
-        if(acc >=n) {
-            const score = getScore(info, data);
-            if(maxScoreGap <= score){
-                if(score === maxScoreGap){                 
-                    if(getMin(answer, data)) answer = data.slice(0);
-                }
-                else answer = data.slice(0);
-                maxScoreGap = score;
-            }
-            return;
-        }
-        
         if(depth > 10){
             return;
         }
         
+        if(acc >= n) {
+            const score = getScore(info, data);
+            if(maxScoreGap <= score){
+                maxScoreGap = score;
+                answer = data.slice(0);
+            }
+            return;
+        }
+        
         data[depth]++;
-        dfs(depth, acc+1);
         dfs(depth+1, acc+1);
+        dfs(depth, acc+1);
         data[depth]--;
         dfs(depth+1, acc)
+      
         return;
     }
     
@@ -50,19 +48,6 @@ function getScore(a, b){
         if(a[i] >= b[i]) scoreA += (10-i);
     }
     
-    return scoreB - scoreA;   
-}
-
-function getMin(a,b){
+    return scoreB - scoreA;
     
-    for(let i = 10; i>=0; i--){
-        if(a[i] !== b[i]) {
-            const max = Math.max(a[i], b[i]);
-            if(max === b[i]) return true;
-            
-            if(max === a[i]) return false;
-        }
-    }
-    
-    return false;
 }
