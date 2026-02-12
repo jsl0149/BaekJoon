18a8207e775d33fb6917721d4deb2670b52135ef
jsl0149 <76390004+jsl0149@users.noreply.github.com>
2026-02-12 23:35:58 +0900

    [Silver I] Title: 곱셈, Time: 92 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.js
+++ 백준/Silver/1629. 곱셈/곱셈.js
@@ -3,25 +3,24 @@ const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const _input = fs.readFileSync(filePath).toString().trim().split('\n');
 
 const solution = (input) => {
-  const [target, exp, rest] = input[0].split(' ').map(Number);
+  const [target, exp, rest] = input[0].split(' ').map(BigInt);
 
   const power = (base, exponent) => {
-    if (exponent === 1) return BigInt(base);
+    if (exponent === 1n) return base;
 
-    if (exponent % 2 === 0) {
-      const half = BigInt(power(BigInt(base), exponent / 2) % BigInt(rest));
-      return (BigInt(half) * BigInt(half)) % BigInt(rest);
+    if (exponent % 2n === 0n) {
+      const half = power(base, exponent / 2n) % rest;
+      return (half * half) % rest;
     }
 
-    if (exponent % 2 === 1) {
-      const half = BigInt(
-        power(BigInt(base), (exponent - 1) / 2) % BigInt(rest),
-      );
-      return (BigInt(half) * BigInt(half) * BigInt(base)) % BigInt(rest);
+    if (exponent % 2n === 1n) {
+      const half = power(base, (exponent - 1n) / 2n) % rest;
+
+      return (half * half * base) % rest;
     }
   };
 
-  const ans = BigInt(power(target, exp)) % BigInt(rest);
+  const ans = power(target, exp) % rest;
 
   console.log(ans.toString());
 };
