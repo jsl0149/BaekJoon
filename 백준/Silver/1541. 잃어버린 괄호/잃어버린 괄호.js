5306cfcbdd51c52bbe9495b13e2826702755a9e3
jsl0149 <76390004+jsl0149@users.noreply.github.com>
2026-02-22 15:24:26 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 92 ms, Memory: 9348 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.js
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.js
@@ -1,26 +1,20 @@
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-const input = fs.readFileSync(filePath).toString().trim().split('\n');
+const _input = fs.readFileSync(filePath).toString().trim().split('\n');
 
-const data = input[0].split('-');
+const solution = (input) => {
+  const str = input[0].split('-');
 
-const temp = [];
+  const exp = [];
 
-for (i of data) {
-  let cnt = 0;
-  let s = i.split('+');
+  str.forEach((row) => {
+    const temp = row.split('+').map(Number);
+    exp.push(temp.reduce((prev, cur) => prev + cur, 0));
+  });
 
-  for (j of s) {
-    cnt += parseInt(j);
-  }
+  const ans = exp.reduce((prev, cur) => prev - cur, exp[0] * 2);
 
-  temp.push(cnt);
-}
+  console.log(ans);
+};
 
-let answer = temp[0];
-
-for (let i = 1; i < temp.length; i++) {
-  answer -= temp[i];
-}
-
-console.log(answer);
+solution(_input);
