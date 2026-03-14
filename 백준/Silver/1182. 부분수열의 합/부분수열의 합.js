a8d91e8cfa289e2b717962af12b6d9a6ea3f2f15
jsl0149 <76390004+jsl0149@users.noreply.github.com>
2026-03-15 03:05:46 +0900

    [Silver II] Title: 부분수열의 합, Time: 164 ms, Memory: 9820 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.js
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.js
@@ -1,25 +1,28 @@
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-const input = fs.readFileSync(filePath).toString().trim().split('\n');
-const [N, S] = input[0].split(' ').map(Number);
-const data = input[1].split(' ').map(Number);
-let answer = 0;
-const ans = [];
-const combination = (total,  k, count) => {   
+const _input = fs.readFileSync(filePath).toString().trim().split('\n');
 
-    if(count === N){
-        return;
+const solution = (input) => {
+  const [N, S] = input[0].split(' ').map(Number);
+  const num = input[1].split(' ').map(Number);
+
+  let ans = 0;
+
+  const dfs = (depth, sum) => {
+    if (depth === N) {
+      if (sum === S) ans++;
+      return;
     }
 
-    else{
-        for(let i = k; i<data.length;i++){
-            if(total+data[i] === S) answer++;
-            combination(total + data[i],  i+1, count+1);
-        }
-    }
+    dfs(depth + 1, sum + num[depth]);
+    dfs(depth + 1, sum);
+  };
 
-}
+  dfs(0, 0);
 
-combination(0, 0, 0);
+  if (S === 0) ans -= 1;
 
-console.log(answer);
+  console.log(ans);
+};
+
+solution(_input);
