32b7c3abcb37e150def9418e18da0ef6e1a7fd0f
jsl0149 <76390004+jsl0149@users.noreply.github.com>
2026-03-17 05:29:41 +0900

    [Silver III] Title: N과 M (5), Time: 188 ms, Memory: 22292 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.js
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.js
@@ -1,57 +1,36 @@
-const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const input = fs.readFileSync(filePath).toString().trim().split("\n");
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const _input = fs.readFileSync(filePath).toString().trim().split('\n');
 
-const solution = (data) => {
-  const [N, M] = data[0].split(" ").map(Number);
-  const num = data[1].split(" ").map(Number);
+const solution = (input) => {
+  const [N, M] = input[0].split(' ').map(Number);
+  const num = input[1].trim().split(' ').map(Number);
   num.sort((a, b) => a - b);
-  let answer = [];
 
-  let output = [];
+  let ans = '';
+  let arr = [];
+  let used = Array(M).fill(false);
 
-  let output2 = [];
-
-  const factorial = (cur, depth) => {
+  const dfs = (depth) => {
     if (depth === M) {
-      const temp = output2.slice(0);
-      answer.push(temp);
+      ans += `${arr.join(' ')}\n`;
       return;
-    } else {
-      for (let i = 0; i < cur.length; i++) {
-        output2.push(cur[i]);
-        const temp = [...cur.slice(0, i), ...cur.slice(i + 1, cur.length)];
-        factorial(temp, depth + 1);
-        output2.pop();
+    }
+
+    for (let i = 0; i < N; i++) {
+      if (!used[i]) {
+        used[i] = true;
+        arr.push(num[i]);
+        dfs(depth + 1);
+        arr.pop();
+        used[i] = false;
       }
     }
   };
 
-  const dfs = (start, depth) => {
-    if (depth === M) {
-      factorial(output, 0);
-      return;
-    } else {
-      for (let i = start; i < num.length; i++) {
-        output.push(num[i]);
-        dfs(i + 1, depth + 1);
-        output.pop();
-      }
-    }
-  };
+  dfs(0);
 
-  dfs(0, 0);
-
-  const compare = (a, b) => {
-    for (let i = 0; i < a.length; i++) {
-      if (a[i] === b[i]) continue;
-      return a[i] - b[i];
-    }
-  };
-
-  answer.sort(compare);
-  answer = answer.map((val) => val.join(" "));
-  return answer.join("\n");
+  console.log(ans.trim());
 };
 
-console.log(solution(input));
+solution(_input);
